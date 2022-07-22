0459221ed57380eee4ba6f6aebdd1559e741222a
JAEHYUNYUK <73768627+JAEHYUNYUK@users.noreply.github.com>
2022-07-22 22:02:26 +0900

    Delete series.py

--- series.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Series 객체 생성
-temp = pd.Series([-20, -10, 10, 20])
-temp[0] # 1월 온도
-
-# Series (Index 지정)
-temp = pd.Series([-20, -10, 10, 20], index = ['Jan', 'Feb', "mar", "Apr"])
-temp['Jan'] # Jan 데이터 출력

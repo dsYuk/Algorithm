1bb6048771f73ef83705774e66c139c34ee6497e
JAEHYUN6 <73768627+JAEHYUN6@users.noreply.github.com>
2023-01-11 15:46:54 +0900

    Add files via upload

---
+++ Programers/lv2/더 맵게.py
@@ -0,0 +1,15 @@
+import heapq
+
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+
+    while scoville[0] < K:
+        new = heapq.heappop(scoville) + heapq.heappop(scoville) * 2
+        heapq.heappush(scoville, new)
+        # print(scoville)
+        answer += 1
+        if len(scoville) == 1 and scoville[0] < K:
+            return -1
+    return answer
